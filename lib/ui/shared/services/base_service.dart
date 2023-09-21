8788eea53646ce7505f51cd3328b586f53e110f7
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-09-21 10:26:16 -0500

    update services base url

--- lib/ui/shared/services/base_service.dart
+++ lib/ui/shared/services/base_service.dart
@@ -1,5 +1,5 @@
 class BaseService {
-  final String baseUri = "https://zendriver.azurewebsites.net";
+  final String baseUri = "https://zendrivers.azurewebsites.net";
   final String apiVersion = "api/v1";
 
   String produceUri(String endUri) => "$baseUri/$apiVersion/$endUri";
