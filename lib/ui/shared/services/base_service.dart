8d63190543aaeb1ea8d2abb42b2fb6d3d3e15e29
Abel Cierto <u20201b561@upc.edu.pe>
2023-09-23 16:51:09 -0500

    feat/regystersuccessfull

--- lib/ui/shared/services/base_service.dart
+++ lib/ui/shared/services/base_service.dart
@@ -1,5 +1,6 @@
 class BaseService {
-  final String baseUri = "https://zendrivers.azurewebsites.net";
+  //final String baseUri = "https://zendrivers.azurewebsites.net";
+  final String baseUri = "https://localhost:7078";
   final String apiVersion = "api/v1";
 
   String produceUri(String endUri) => "$baseUri/$apiVersion/$endUri";
